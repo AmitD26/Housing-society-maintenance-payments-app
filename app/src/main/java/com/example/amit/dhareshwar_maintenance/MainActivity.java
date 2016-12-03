bfbb27451a5f2bf78cdad50fb61fba26562175ce
AmitD26 <amitdharmadhikari41@gmail.com>
2016-12-03 22:45:39 +0530

    A basic login activity.

--- app/src/main/java/com/example/amit/dhareshwar_maintenance/MainActivity.java
+++ app/src/main/java/com/example/amit/dhareshwar_maintenance/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.amit.dhareshwar_maintenance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+   }
+
+    public void loginButtonClicked(View v) {
+        String username = this.username.getText().toString();
+        String password = this.password.getText().toString();
+        try {
+            String response = new ServerConnection(this).execute(username,password).get();
+            Log.e("response",response);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
