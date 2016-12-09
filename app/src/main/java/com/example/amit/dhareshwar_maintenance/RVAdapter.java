bca2389e38b737bfdcdc828d8d34f813729d70ff
AmitD26 <amitdharmadhikari41@gmail.com>
2016-12-09 21:22:09 +0530

    Junk

--- app/src/main/java/com/example/amit/dhareshwar_maintenance/RVAdapter.java
+++ app/src/main/java/com/example/amit/dhareshwar_maintenance/RVAdapter.java
@@ -4,7 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by amit on 9/12/16.
@@ -12,9 +14,17 @@ import android.widget.TextView;
 
 public class RVAdapter extends RecyclerView.Adapter {
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_notices,parent,false);
         NoticeViewHolder nvh = new NoticeViewHolder(v);
+        nvh.rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(parent.getContext(), "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        
         return nvh;
     }
 
@@ -30,10 +40,12 @@ public class RVAdapter extends RecyclerView.Adapter {
 
     public static class NoticeViewHolder extends RecyclerView.ViewHolder {
         TextView hello, hello1;
+        RelativeLayout rl;
         public NoticeViewHolder(View itemView) {
             super(itemView);
             hello = (TextView) itemView.findViewById(R.id.card);
             hello1 = (TextView) itemView.findViewById(R.id.card1);
+            rl = (RelativeLayout) itemView.findViewById(R.id.noticeRL);
         }
     }
 }
