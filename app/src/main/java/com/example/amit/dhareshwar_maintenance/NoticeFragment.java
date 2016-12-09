7b0f1728448cb7651fa400ce1d383f28afc40e66
AmitD26 <amitdharmadhikari41@gmail.com>
2016-12-09 18:25:56 +0530

    Junk

--- app/src/main/java/com/example/amit/dhareshwar_maintenance/NoticeFragment.java
+++ app/src/main/java/com/example/amit/dhareshwar_maintenance/NoticeFragment.java
@@ -1,6 +1,7 @@
 package com.example.amit.dhareshwar_maintenance;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -96,7 +97,7 @@ public class NoticeFragment extends Fragment {
         showNoticeDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(getActivity(), ScrollingActivity.class));
             }
         });
 
